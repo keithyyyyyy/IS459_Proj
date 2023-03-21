ac2d0f5174436d67d007ba47440b0c31fcd2c11f
Keith Tan GF <dirtypupkeith@live.com.sg>
2023-03-21 20:57:18 +0800

    Update nodeflair_scraper2.py

--- nodeflair_scraper2.py
+++ nodeflair_scraper2.py
@@ -17,48 +17,49 @@ def scrape_jobs(url):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    job_data = [{"test": "test value"}]
+    # job_data = [{"test": "test value"}]
+    job_data = []
 
-    # while True:
-    #     try:
-    #         WebDriverWait(driver, 10).until(
-    #             EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'jobListingCardsContainer')]"))
-    #         )
-    #         jobs = driver.find_element(By.XPATH, "//div[contains(@class, 'jobListingCardsContainer')]")
+    while True:
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'jobListingCardsContainer')]"))
+            )
+            jobs = driver.find_element(By.XPATH, "//div[contains(@class, 'jobListingCardsContainer')]")
 
-    #         jobs = jobs.find_elements(By.XPATH, "//div[contains(@class, 'jobListingCard-')]")
+            jobs = jobs.find_elements(By.XPATH, "//div[contains(@class, 'jobListingCard-')]")
 
-    #         for job in jobs:
-    #             title = job.find_element(By.XPATH, ".//h2[contains(@class, 'jobListingCardTitle')]").text
-    #             company = job.find_element(By.XPATH, ".//p[contains(@class, 'companynameAndRating')]/span").text
-    #             country = job.find_element(By.XPATH, ".//div[contains(@class, 'country')]").text
-    #             stacks = job.find_elements(By.XPATH, ".//span[contains(@class, 'techStackContainer')]")
-    #             stacks_array = []
-    #             for stack in stacks:
-    #                 stacks_array.append(stack.text)
-    #             salary = job.find_elements(By.XPATH, ".//span[contains(@class, 'salary')]")
-    #             salary_amt = "Empty"
-    #             if salary:
-    #                 salary_amt = salary[0].text
-    #             job_data.append({
-    #                 'company': company,
-    #                 'title': title,
-    #                 'country': country,
-    #                 'stacks': stacks_array,
-    #                 'salary': salary_amt
-    #             })
+            for job in jobs:
+                title = job.find_element(By.XPATH, ".//h2[contains(@class, 'jobListingCardTitle')]").text
+                company = job.find_element(By.XPATH, ".//p[contains(@class, 'companynameAndRating')]/span").text
+                country = job.find_element(By.XPATH, ".//div[contains(@class, 'country')]").text
+                stacks = job.find_elements(By.XPATH, ".//span[contains(@class, 'techStackContainer')]")
+                stacks_array = []
+                for stack in stacks:
+                    stacks_array.append(stack.text)
+                salary = job.find_elements(By.XPATH, ".//span[contains(@class, 'salary')]")
+                salary_amt = "Empty"
+                if salary:
+                    salary_amt = salary[0].text
+                job_data.append({
+                    'company': company,
+                    'title': title,
+                    'country': country,
+                    'stacks': stacks_array,
+                    'salary': salary_amt
+                })
 
-    #         next_button = driver.find_elements(By.XPATH, "//a[contains(@class, 'page-link')]")[5]
+            next_button = driver.find_elements(By.XPATH, "//a[contains(@class, 'page-link')]")[5]
 
-    #         if 'disabled' in next_button.get_attribute('class'):
-    #             break
-    #         else:
-    #             next_button.click()
-    #             time.sleep(2)
+            if 'disabled' in next_button.get_attribute('class'):
+                break
+            else:
+                next_button.click()
+                time.sleep(2)
 
-    #     except Exception as e:
-    #         print(f"Error occurred: {e}")
-    #         break
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            break
 
     driver.quit()
 
