585bcc77fd85a8ee15183c581bb1a755172221a7
lowqilong <lowqilong@hotmail.com>
2023-03-26 01:01:30 +0800

    changes

--- nodeflair-scraper.py
+++ nodeflair-scraper.py
@@ -10,12 +10,7 @@ def scrape_jobs(url):
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--start-fullscreen')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--enable-logging')
-    chrome_options.add_argument('--log-level=0')
-    chrome_options.add_argument('--verbose')
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
@@ -34,14 +29,13 @@ def scrape_jobs(url):
 
             for job in jobs:
                 job.click()
-                WebDriverWait(driver, 5).until(
+                WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'jobDescriptionContent')]"))
                 )
-                title = job.find_element(By.XPATH, ".//h2[contains(@class, 'jobListingCardTitle')]").text
-                company = job.find_element(By.XPATH, ".//p[contains(@class, 'companynameAndRating')]/span").text
-                country = job.find_element(By.XPATH, ".//div[contains(@class, 'country')]").text
+                title = job.find_elements(By.XPATH, ".//h2[contains(@class, 'jobListingCardTitle')]")[0].text
+                company = job.find_elements(By.XPATH, ".//p[contains(@class, 'companynameAndRating')]/span")[0].text
                 stacks = job.find_elements(By.XPATH, ".//span[contains(@class, 'techStackContainer')]")
-                description = job.find_element(By.XPATH, "//div[contains(@class, 'jobDescriptionContent')]").text
+                description = job.find_elements(By.XPATH, "//div[contains(@class, 'jobDescriptionContent')]")[0].text
                 stacks_array = []
                 for stack in stacks:
                     stacks_array.append(stack.text)
@@ -52,7 +46,6 @@ def scrape_jobs(url):
                 job_data.append({
                     'company': company,
                     'title': title,
-                    'country': country,
                     'stacks': stacks_array,
                     'salary': salary_amt,
                     'description': description
@@ -63,7 +56,7 @@ def scrape_jobs(url):
             next_button = driver.find_elements(By.XPATH, "//a[contains(@class, 'page-link')]")[-2]
             print(current_button)
 
-            if 'disabled' in next_button.get_attribute('class'):
+            if next_button.text != "⟩":
                 break
             else:
                 next_button.click()
@@ -78,7 +71,7 @@ def scrape_jobs(url):
     return job_data
 
 if __name__ == "__main__":
-    url = "https://nodeflair.com/jobs?query=software&page=1&sort_by=relevant&positions%5B%5D=full_stack_developer&positions%5B%5D=backend_developer&positions%5B%5D=frontend_developer&positions%5B%5D=gaming_engineer&positions%5B%5D=blockchain_engineer&positions%5B%5D=android_developer&positions%5B%5D=ios_developer&positions%5B%5D=mobile_developer&positions%5B%5D=data_analyst&positions%5B%5D=data_engineer&positions%5B%5D=data_scientist&positions%5B%5D=algorithm_engineer&positions%5B%5D=devops_engineer&positions%5B%5D=reliability_engineer&positions%5B%5D=sysops_engineer&positions%5B%5D=qa_engineer&positions%5B%5D=cybersecurity_engineer&positions%5B%5D=cybersecurity_operations&seniorities%5B%5D=junior&countries%5B%5D=Singapore"
+    url = "https://nodeflair.com/jobs?query=software&page=35&sort_by=relevant&positions%5B%5D=full_stack_developer&positions%5B%5D=backend_developer&positions%5B%5D=frontend_developer&positions%5B%5D=gaming_engineer&positions%5B%5D=blockchain_engineer&positions%5B%5D=android_developer&positions%5B%5D=ios_developer&positions%5B%5D=mobile_developer&positions%5B%5D=data_analyst&positions%5B%5D=data_engineer&positions%5B%5D=data_scientist&positions%5B%5D=algorithm_engineer&positions%5B%5D=devops_engineer&positions%5B%5D=reliability_engineer&positions%5B%5D=sysops_engineer&positions%5B%5D=qa_engineer&positions%5B%5D=cybersecurity_engineer&positions%5B%5D=cybersecurity_operations&seniorities%5B%5D=junior&countries%5B%5D=Singapore"
     jobs = scrape_jobs(url)
 
     with open("jobs.json", "w") as outfile:
