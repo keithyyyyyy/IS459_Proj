d004a80e4911cfc27eb684eeebed45a0060cb262
Keith Tan GF <dirtypupkeith@live.com.sg>
2023-03-26 11:46:34 +0800

    Update nodeflair-scraper.py

--- nodeflair-scraper.py
+++ nodeflair-scraper.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -5,6 +6,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import json
+import boto3
+import os
+
+load_dotenv()
+
+access_key_id = os.getenv("ACCESS_KEY_ID")
+secret_access_key = os.getenv("SECRET_ACCESS_KEY")
 
 def scrape_jobs(url):
     chrome_options = Options()
@@ -56,11 +64,12 @@ def scrape_jobs(url):
             next_button = driver.find_elements(By.XPATH, "//a[contains(@class, 'page-link')]")[-2]
             print(current_button)
 
-            if next_button.text != "⟩":
-                break
-            else:
-                next_button.click()
-                time.sleep(2)
+            break
+            # if next_button.text != "⟩":
+            #     break
+            # else:
+            #     next_button.click()
+            #     time.sleep(2)
 
         except Exception as e:
             print(f"Error occurred: {e}")
@@ -74,5 +83,22 @@ if __name__ == "__main__":
     url = "https://nodeflair.com/jobs?query=software&page=1&sort_by=relevant&positions%5B%5D=full_stack_developer&positions%5B%5D=backend_developer&positions%5B%5D=frontend_developer&positions%5B%5D=gaming_engineer&positions%5B%5D=blockchain_engineer&positions%5B%5D=android_developer&positions%5B%5D=ios_developer&positions%5B%5D=mobile_developer&positions%5B%5D=data_analyst&positions%5B%5D=data_engineer&positions%5B%5D=data_scientist&positions%5B%5D=algorithm_engineer&positions%5B%5D=devops_engineer&positions%5B%5D=reliability_engineer&positions%5B%5D=sysops_engineer&positions%5B%5D=qa_engineer&positions%5B%5D=cybersecurity_engineer&positions%5B%5D=cybersecurity_operations&seniorities%5B%5D=junior&countries%5B%5D=Singapore"
     jobs = scrape_jobs(url)
 
-    with open("jobs.json", "w") as outfile:
-        json.dump(jobs, outfile)
+    s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+    bucket_name = 'is459-t3-job-raw-data'
+    file_name = 'jobs.json'
+    json_string = json.dumps(jobs)
+
+    s3.put_object(
+        Bucket=bucket_name,
+        Key='nodeflair_raw/' + file_name,
+        Body=json_string.encode('utf-8')
+    )
+
+
+    # with open("jobs.json", "w") as outfile:
+        # json.dump(jobs, outfile)
+    
+    
+    
+
+
